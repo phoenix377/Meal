1792cdd141465ba7fc93a7f35c1292c5148883fc
phoenix377 <phoenix.number377@gmail.com>
2016-04-04 08:53:02 +0800

    add user for administrator

--- client/app/users/users.controller.js
+++ client/app/users/users.controller.js
@@ -4,6 +4,7 @@
 
 class UserController {
   constructor(User) {
+    this.User = User;
     // Use the User $resource to fetch all users
     this.users = User.query();
   }
@@ -11,6 +12,20 @@ class UserController {
   delete(user) {
     user.$remove();
     this.users.splice(this.users.indexOf(user), 1);
+  }
+  
+  add(email, name, password) {
+    var thisCtrl = this;
+    this.submitted = true;
+    this.User.save({
+      name: name,
+      email: email,
+      password: password
+    },function(data) {
+      thisCtrl.users = thisCtrl.User.query();
+    }, function(err) {
+      return safeCb(callback)(err);
+    });
   }
 }
 
